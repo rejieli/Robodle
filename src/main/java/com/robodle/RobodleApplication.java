31f0aae0e344d34432d25a6a427b9afd34c73233
Derek <rejie.li@gmail.com>
2022-04-25 14:12:44 -0400

    INITIAL COMMIT

---
+++ src/main/java/com/robodle/RobodleApplication.java
@@ -0,0 +1,13 @@
+package com.robodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobodleApplication.class, args);
+	}
+
+}
